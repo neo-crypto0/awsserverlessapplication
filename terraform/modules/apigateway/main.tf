a90f6c0ee80e06e62d7e4be79b12797696570046
neo-crypto0 <neo.crypto000@gmail.com>
2025-06-26 02:43:38 +0530

    no changes

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -92,3 +92,4 @@ resource "aws_api_gateway_stage" "prod" {
 output "api_invoke_url" {
   value = "https://${aws_api_gateway_rest_api.employee_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}/"
 }
+
